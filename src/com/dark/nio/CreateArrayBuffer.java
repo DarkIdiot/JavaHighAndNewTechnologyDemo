8632bc300213851251dd82d61d9ba324291f814f
DarkIdiot <darkidiot@icloud.com>
2016-03-05 00:05:18 +0800

    temp update.

--- src/com/dark/nio/CreateArrayBuffer.java
+++ src/com/dark/nio/CreateArrayBuffer.java
@@ -4,21 +4,20 @@ import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
 
-public class CreateArrayBuffer
-{
-  static public void main( String args[] ) throws Exception {
-    byte array[] = new byte[1024];
+public class CreateArrayBuffer {
+	static public void main(String args[]) throws Exception {
+		byte array[] = new byte[1024];
 
-    ByteBuffer buffer = ByteBuffer.wrap( array );
+		ByteBuffer buffer = ByteBuffer.wrap(array);
 
-    buffer.put( (byte)'a' );
-    buffer.put( (byte)'b' );
-    buffer.put( (byte)'c' );
+		buffer.put((byte) 'a');
+		buffer.put((byte) 'b');
+		buffer.put((byte) 'c');
 
-    buffer.flip();
+		buffer.flip();
 
-    System.out.println( (char)buffer.get() );
-    System.out.println( (char)buffer.get() );
-    System.out.println( (char)buffer.get() );
-  }
+		System.out.println((char) buffer.get());
+		System.out.println((char) buffer.get());
+		System.out.println((char) buffer.get());
+	}
 }
