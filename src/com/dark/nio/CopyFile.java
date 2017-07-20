bc041ae2aca9356618464962cef99d8bd1b36bcd
heqiao <heqiao2@gome.com.cn>
2017-07-20 20:36:18 +0800

    commit code.

--- src/com/dark/nio/CopyFile.java
+++ src/com/dark/nio/CopyFile.java
@@ -3,6 +3,7 @@ package com.dark.nio;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 import java.nio.channels.FileChannel;
 
 public class CopyFile {
@@ -18,7 +19,6 @@ public class CopyFile {
 
 		FileInputStream fin = new FileInputStream(infile);
 		FileOutputStream fout = new FileOutputStream(outfile);
-
 		FileChannel fcin = fin.getChannel();
 		FileChannel fcout = fout.getChannel();
 
@@ -26,15 +26,11 @@ public class CopyFile {
 
 		while (true) {
 			buffer.clear();
-
 			int r = fcin.read(buffer);
-
 			if (r == -1) {
 				break;
 			}
-
 			buffer.flip();
-
 			fcout.write(buffer);
 		}
 	}
