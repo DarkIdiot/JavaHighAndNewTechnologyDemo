0fec6bf699f40d799e9b54dbd3f6cdc3e0626bf0
heqiao <heqiao2@gome.com.cn>
2017-07-28 20:53:48 +0800

    commit code.

--- src/com/dark/jvm/InstructionReorderingDemo.java
+++ src/com/dark/jvm/InstructionReorderingDemo.java
@@ -1,7 +1,5 @@
 package com.dark.jvm;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * 指令重排-破坏线程间的有序性
  * 
@@ -27,27 +25,26 @@ public class InstructionReorderingDemo {
 	 * 线程A首先执行writer()方法,
 	 * 线程B线程接着执行reader()方法,
 	 * 线程B在int i=a+1 是不一定能看到a已经被赋值为1,
-	 * 因为在writer中，两句话顺序可能打乱
+	 * 因为在writer中，两句话顺序可能打乱 (JDK8很难重试)
 	 * @param args
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		CountDownLatch countDownLatch = new CountDownLatch(2);
 		InstructionReorderingDemo instructionReorderingDemo = new InstructionReorderingDemo();
-		new Thread(new Runnable() {
+		Thread thread1 = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				instructionReorderingDemo.writer();
-				countDownLatch.countDown();
 			}
-		}).start();
-		new Thread(new Runnable() {
+		});
+		Thread thread2 = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				instructionReorderingDemo.reader();
-				countDownLatch.countDown();
 			}
-		}).start();
-		countDownLatch.await();
+		});
+		thread1.start();
+		thread2.start();
+		Thread.sleep(10);
 	}
 }
